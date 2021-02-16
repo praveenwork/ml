995f9d94d549ae584b86ce0f5b38bf8d70463aa1
praveenwork <ppasupuleti@prosper.com>
2021-02-15 17:16:40 -0800

    downloadFiles

--- Python/files/DownLoadFile.py
+++ Python/files/DownLoadFile.py
@@ -3,5 +3,5 @@ import requests
 url = "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/sarcasm.json"
 req = requests.get(url)
 
-open("sarcasm.json", "wb").write(req.content)
+open("../nlp/sarcasm.json", "wb").write(req.content)
 
