f36869c9fe200d8b5a537d6bda85c99197e37d94
praveenwork <ppasupuleti@prosper.com>
2021-02-15 20:30:29 -0800

    stopwords

--- Python/files/DownLoadFile.py
+++ Python/files/DownLoadFile.py
@@ -1,7 +1,12 @@
 import requests
 
-url = "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/sarcasm.json"
-req = requests.get(url)
+def download_file(url, filepath):
+    req = requests.get(url)
+    open(filepath, "wb").write(req.content)
 
-open("../nlp/sarcasm.json", "wb").write(req.content)
 
+# sarcasm data
+#download_file("https://storage.googleapis.com/laurencemoroney-blog.appspot.com/sarcasm.json", "../nlp/sarcasm1.json")
+
+# BBC DataSet
+download_file("https://storage.googleapis.com/laurencemoroney-blog.appspot.com/bbc-text.csv"    , "../nlp/bbc-text.csv")
